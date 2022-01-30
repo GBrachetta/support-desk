2056d2f3deccdf5b8341bf69d6f9d6e2a4b9f6fb
Guillermo Brachetta <brachetta@me.com>
2022-01-30 21:17:05 +0100

    Create tickets from frontend

---
+++ frontend/src/features/tickets/ticketSlice.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import ticketService from './ticketService';
+
+const initialState = {
+  tickets: [],
+  ticket: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// Create ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkApi) => {
+    try {
+      const { token } = thunkApi.getState().auth.user;
+
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkApi.rejectWithValue(message);
+    }
+  },
+);
+
+export const ticketSlice = createSlice({
+  name: 'ticket',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = ticketSlice.actions;
+
+export default ticketSlice.reducer;
