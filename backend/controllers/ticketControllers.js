a634803b25b7a2e630570a0516c05e6e9e590d0b
Guillermo Brachetta <brachetta@me.com>
2022-01-30 16:16:54 +0100

    Add all CRUD for tickets

--- backend/controllers/ticketControllers.js
+++ backend/controllers/ticketControllers.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-
+const mongoose = require('mongoose');
 const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
 
@@ -7,14 +7,163 @@ const Ticket = require('../models/ticketModel');
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get Tickets' });
+  // Get current token from the token
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
+});
+
+// @desc    Get user ticket
+// @route   GET /api/tickets/:id
+// @access  Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get current token from the token
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // Check if ticket is a valid ObjectId
+  const ticketIsValid = mongoose.isValidObjectId(req.params.id);
+
+  if (!ticketIsValid) {
+    res.status(400);
+    throw new Error('Ticket number not valid');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
 });
 
 // @desc    Crate new ticket
 // @route   POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'create Ticket' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add a product and description');
+  }
+
+  // Get current token from the token
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+  res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+// @desc    Delete ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get current token from the token
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // Check if ticket is a valid ObjectId
+  const ticketIsValid = mongoose.isValidObjectId(req.params.id);
+
+  if (!ticketIsValid) {
+    res.status(400);
+    throw new Error('Ticket number not valid');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(202).json({ success: true });
+});
+
+// @desc    Update ticket
+// @route   PUT /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get current token from the token
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // Check if ticket is a valid ObjectId
+  const ticketIsValid = mongoose.isValidObjectId(req.params.id);
+
+  if (!ticketIsValid) {
+    res.status(400);
+    throw new Error('Ticket number not valid');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(201).json(updatedTicket);
+});
+
+module.exports = {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,8 +4,17 @@ const { protect } = require('../middleware/authMiddleware');
 const {
   getTickets,
   createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketControllers');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
