af17e372701c3f423dc08715d101224b70c731ab
Guillermo Brachetta <brachetta@me.com>
2022-01-31 19:08:55 +0100

    Get user tickets and put them in the state.

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -33,11 +33,34 @@ export const createTicket = createAsyncThunk(
   },
 );
 
+// Get user tickets
+export const getTickets = createAsyncThunk(
+  'tickets/getAll',
+  async (_, thunkApi) => {
+    try {
+      const { token } = thunkApi.getState().auth.user;
+
+      return await ticketService.getTickets(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkApi.rejectWithValue(message);
+    }
+  },
+);
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
   reducers: {
     reset: (state) => {
+      state.tickets = [];
+      state.ticket = {};
       state.isError = false;
       state.isSuccess = false;
       state.isLoading = false;
@@ -54,6 +77,19 @@ export const ticketSlice = createSlice({
         state.isSuccess = true;
       })
       .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getTickets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTickets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tickets = action.payload;
+      })
+      .addCase(getTickets.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
