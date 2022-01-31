cdd68b0230b49720035b9988f42bf3b53f78d274
Guillermo Brachetta <brachetta@me.com>
2022-01-31 19:25:56 +0100

    Implement getTickets page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import TicketItem from '../components/TicketItem';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 
 const Tickets = () => {
@@ -25,7 +26,24 @@ const Tickets = () => {
 
   if (isLoading) return <Spinner />;
 
-  return <div>a</div>;
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div />
+        </div>
+        {tickets.map((ticket) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Tickets;
